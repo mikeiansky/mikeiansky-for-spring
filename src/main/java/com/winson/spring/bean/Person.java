82d251795c12cbc9c4e5e818ae91d92533c2f900
winson@home <929566854@qq.com>
2020-12-20 22:08:16 +0800

    init project;

---
+++ src/main/java/com/winson/spring/bean/Person.java
@@ -0,0 +1,13 @@
+package com.winson.spring.bean;
+
+/**
+ * @author winson
+ * @date 2020/12/20
+ **/
+public class Person {
+
+    public void sayHello(){
+        System.out.println("Person say hello.");
+    }
+
+}
---
+++ src/test/java/com/winson/spring/TestSpring.java
@@ -0,0 +1,21 @@
+package com.winson.spring;
+
+import com.winson.spring.bean.Person;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author winson
+ * @date 2020/12/20
+ **/
+public class TestSpring {
+
+    @Test
+    public void testHelloSpring(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("hello-spring.xml");
+        Person person = context.getBean(Person.class);
+        person.sayHello();
+    }
+
+}
