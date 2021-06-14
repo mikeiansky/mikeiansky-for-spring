60e1fc70202828adb734c551495b3d0020dee00d
winson@home <929566854@qq.com>
2021-06-15 07:13:56 +0800

    新增java-beans-demo测试模块;

---
+++ java-beans-demo/src/main/java/com/winson/java/beans/demo/TestJavaBeansDemo.java
@@ -0,0 +1,15 @@
+package com.winson.java.beans.demo;
+
+/**
+ * @author winson
+ * @date 2021/6/15
+ **/
+public class TestJavaBeansDemo {
+
+    public static void main(String[] args) {
+        System.out.println("test java beans demo start ... ");
+
+        System.out.println("test java beans demo stop ... ");
+    }
+
+}
