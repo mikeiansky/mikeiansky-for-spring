6deec68edeb1b88284ab80b4869ef4667e60e7b6
winson@home <929566854@qq.com>
2021-06-19 10:36:41 +0800

    测试Bean的初始化;

--- spring-beans/src/main/java/com/winson/study/spring/beans/factory/DefaultUserFactory.java
+++ spring-beans/src/main/java/com/winson/study/spring/beans/factory/DefaultUserFactory.java
@@ -1,11 +1,28 @@
 package com.winson.study.spring.beans.factory;
 
 import com.winson.study.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author winson
  * @date 2021/6/19
  **/
-public class DefaultUserFactory implements UserFactory{
+public class DefaultUserFactory implements UserFactory, InitializingBean {
+
+    public void initMethod() {
+        System.out.println("this is init method ... ");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("after construct ... ");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("after properties set ... ");
+    }
 
 }
