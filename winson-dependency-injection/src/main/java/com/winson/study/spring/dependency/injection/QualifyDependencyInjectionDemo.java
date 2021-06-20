570702f415a81d16bd0be5cd875c973aae8e0775
winson@home <929566854@qq.com>
2021-06-20 12:49:01 +0800

    自定义依赖注入;

--- winson-dependency-injection/src/main/java/com/winson/study/spring/dependency/injection/QualifyDependencyInjectionDemo.java
+++ winson-dependency-injection/src/main/java/com/winson/study/spring/dependency/injection/QualifyDependencyInjectionDemo.java
@@ -1,14 +1,25 @@
 package com.winson.study.spring.dependency.injection;
 
+import com.winson.study.spring.dependency.injection.annotation.InjectionUser;
+import com.winson.study.spring.dependency.injection.annotation.MyAutowired;
 import com.winson.study.spring.dependency.injection.annotation.UserGroup;
 import com.winson.study.spring.ioc.overview.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
+import java.lang.annotation.Annotation;
 import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+import static org.springframework.context.annotation.AnnotationConfigUtils.AUTOWIRED_ANNOTATION_PROCESSOR_BEAN_NAME;
 
 /**
  * @author winson
@@ -22,46 +33,67 @@ public class QualifyDependencyInjectionDemo {
         return user;
     }
 
-    @Autowired
-    public Collection<User> allUser;
+//    @Autowired
+//    public Collection<User> allUser;
+
+//    @Autowired
+//    @Qualifier
+//    public Collection<User> qualifierUser;
+
+//    @Autowired
+//    @UserGroup
+//    public Collection<User> userGroupUser;
 
     @Autowired
-    @Qualifier
-    public Collection<User> qualifierUser;
-
-    @Autowired
-    @UserGroup
-    public Collection<User> userGroupUser;
-
-    @Autowired
-    @Qualifier("user")
+//    @Qualifier("user")
     public User normalUser;
 
-    @Autowired
+    @MyAutowired
     public User superUser;
 
-    @Bean
-    @Qualifier
-    public User u6() {
-        return createUser(6);
-    }
+    @InjectionUser
+    public User injectionUser;
+
+//    @Bean
+//    @Qualifier
+//    public User u6() {
+//        return createUser(6);
+//    }
+//
+//    @Bean
+//    @Qualifier
+//    public User u7() {
+//        return createUser(7);
+//    }
+//
+//    @Bean
+//    @UserGroup
+//    public User u8() {
+//        return createUser(8);
+//    }
+//
+//    @Bean
+//    @UserGroup
+//    public User u9() {
+//        return createUser(9);
+//    }
+
+//    @Bean(name = AUTOWIRED_ANNOTATION_PROCESSOR_BEAN_NAME)
+//    public static AutowiredAnnotationBeanPostProcessor beanPostProcessor() {
+//        AutowiredAnnotationBeanPostProcessor beanPostProcessor = new AutowiredAnnotationBeanPostProcessor();
+//        // @Autowired + @Inject +  新注解 @InjectedUser
+//        Set<Class<? extends Annotation>> autowiredAnnotationTypes =
+//                new LinkedHashSet<>(asList(Autowired.class , InjectionUser.class));
+//        beanPostProcessor.setAutowiredAnnotationTypes(autowiredAnnotationTypes);
+//        return beanPostProcessor;
+//    }
 
     @Bean
-    @Qualifier
-    public User u7() {
-        return createUser(7);
-    }
-
-    @Bean
-    @UserGroup
-    public User u8() {
-        return createUser(8);
-    }
-
-    @Bean
-    @UserGroup
-    public User u9() {
-        return createUser(9);
+    @Order(Ordered.LOWEST_PRECEDENCE - 3)
+    public static AutowiredAnnotationBeanPostProcessor beanPostProcessor() {
+        AutowiredAnnotationBeanPostProcessor beanPostProcessor = new AutowiredAnnotationBeanPostProcessor();
+        beanPostProcessor.setAutowiredAnnotationType(InjectionUser.class);
+        return beanPostProcessor;
     }
 
 
@@ -79,9 +111,10 @@ public class QualifyDependencyInjectionDemo {
         QualifyDependencyInjectionDemo demo = applicationContext.getBean(QualifyDependencyInjectionDemo.class);
         System.out.println("normal user : " + demo.normalUser);
         System.out.println("super user : " + demo.superUser);
-        System.out.println("all user : " + demo.allUser);
-        System.out.println("qualifier user : " + demo.qualifierUser);
-        System.out.println("user group user : " + demo.userGroupUser);
+        System.out.println("injection user : " + demo.injectionUser);
+//        System.out.println("all user : " + demo.allUser);
+//        System.out.println("qualifier user : " + demo.qualifierUser);
+//        System.out.println("user group user : " + demo.userGroupUser);
 
 
         applicationContext.close();
---
+++ winson-dependency-injection/src/main/java/com/winson/study/spring/dependency/injection/annotation/InjectionUser.java
@@ -0,0 +1,15 @@
+package com.winson.study.spring.dependency.injection.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.lang.annotation.*;
+
+/**
+ * @author winson
+ * @date 2021/6/20
+ **/
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface InjectionUser {
+}
