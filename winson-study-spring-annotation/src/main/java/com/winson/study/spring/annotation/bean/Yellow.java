a62c25c8da92d15f8b227b98d1fc4394a6372368
zhouwenxiang <zhouwenxiang@shixiba.com>
2021-09-23 14:13:32 +0800

    clear unused module

--- winson-study-spring-annotation/src/main/java/com/winson/study/spring/annotation/bean/Yellow.java
+++
@@ -1,8 +0,0 @@
-package com.winson.study.spring.annotation.bean;
-
-/**
- * @author winson
- * @date 2021/7/16
- **/
-public class Yellow {
-}
--- winson-study-spring-aop-v1/src/main/java/com/winson/study/spring/aop/v1/annotation/Admin.java
+++
@@ -1,8 +0,0 @@
-package com.winson.study.spring.aop.v1.annotation;
-
-/**
- * @author winson
- * @date 2021/7/19
- **/
-public @interface Admin {
-}
