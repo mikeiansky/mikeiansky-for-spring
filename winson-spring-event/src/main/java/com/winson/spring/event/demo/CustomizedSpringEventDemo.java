f3e4cd33554f9aca1139bb3012888f909767a85f
winson <929566854@qq.com>
2022-04-04 11:42:46 +0800

    spring-event 事件处理 原理分析

--- winson-spring-event/src/main/java/com/winson/spring/event/demo/CustomizedSpringEventDemo.java
+++ winson-spring-event/src/main/java/com/winson/spring/event/demo/CustomizedSpringEventDemo.java
@@ -1,5 +1,8 @@
 package com.winson.spring.event.demo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -7,6 +10,8 @@ import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.context.event.EventListener;
+
+import java.util.Arrays;
 
 /**
  * @author winson
@@ -19,17 +24,26 @@ public class CustomizedSpringEventDemo {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.register(CustomizedSpringEventDemo.class);
         context.addApplicationListener(new MySpringEventListener());
+        context.addBeanFactoryPostProcessor(new BeanFactoryPostProcessor() {
+            @Override
+            public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+                System.out.println("CustomizedSpringEventDemo ==> postProcessBeanFactory :: publish event!");
+                context.publishEvent(new MySpringEvent("before spring context refresh event"));
+            }
+        });
         context.addApplicationListener(new ApplicationListener<ApplicationEvent>() {
             @Override
             public void onApplicationEvent(ApplicationEvent event) {
                 System.out.println("****** event : " + event);
             }
         });
-
+        // error
+//        context.publishEvent(new MySpringEvent("before spring context refresh event"));
         context.refresh();
         context.start();
 
         context.publishEvent(new MySpringEvent("hello event"));
+        context.publishEvent(new MySpringEvent(Arrays.asList(1,2,3,4)));
 
         context.close();
 
