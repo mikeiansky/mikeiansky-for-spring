f44fbc93e9be9d8fd291deb841b713ef92bcb595
zhouwenxiang <zhouwenxiang@shixiba.com>
2022-04-13 10:13:13 +0800

    resolvable测试

--- winson-spring-utils/src/main/java/com/winson/spring/utils/ResolvableTypeDemo.java
+++ winson-spring-utils/src/main/java/com/winson/spring/utils/ResolvableTypeDemo.java
@@ -2,17 +2,21 @@ package com.winson.spring.utils;
 
 import org.springframework.core.ResolvableType;
 
+import java.lang.reflect.Field;
+
 /**
  * @author winson
  * @date 2022/3/5
  **/
 public class ResolvableTypeDemo {
 
+    public String name;
+
     public void sayHello(String msg){
         System.out.println("ResolvableTypeDemo sayHello : " + msg);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException {
 
         ResolvableType type = ResolvableType.forClass(ResolvableTypeDemo.class);
         System.out.println("type.getRawClass() :"+type.getRawClass());
@@ -20,6 +24,8 @@ public class ResolvableTypeDemo {
         System.out.println("type.getComponentType() :"+type.getComponentType());
         System.out.println("type.getInterfaces().length :"+type.getInterfaces().length);
 
+        Field field = ResolvableTypeDemo.class.getDeclaredField("name");
+        System.out.println(ResolvableType.forField(field));
     }
 
 }
