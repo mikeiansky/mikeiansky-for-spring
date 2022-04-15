79ca12093e9c638a68b3c9cc8f6d8bfb585284a9
zhouwenxiang <zhouwenxiang@shixiba.com>
2022-04-15 10:46:45 +0800

    spring-environment测试

---
+++ winson-spring-environment/src/main/java/com/winson/spring/environment/IOUtils.java
@@ -0,0 +1,29 @@
+package com.winson.spring.environment;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * @author winson
+ * @date 2021/10/2
+ **/
+public class IOUtils {
+
+    public static String toString(Reader reader) {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(reader);
+        String line = null;
+        try {
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+                sb.append("\r\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+
+    }
+
+}
