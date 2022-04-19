bb7b727076e899bd3558273f2abb325ab8137216
zhouwenxiang <zhouwenxiang@shixiba.com>
2022-04-19 19:13:27 +0800

    spring-事务测试

--- winson-spring-aop-features_v2/src/main/java/com/winson/spring/aop/features/v2/AspectJAnnotationDemo.java
+++ winson-spring-aop-features_v2/src/main/java/com/winson/spring/aop/features/v2/AspectJAnnotationDemo.java
@@ -2,9 +2,13 @@ package com.winson.spring.aop.features.v2;
 
 import com.winson.spring.aop.features.v2.aspect.AspectConfiguration;
 import com.winson.spring.aop.features.v2.condition.MyCondition;
+import com.winson.spring.aop.features.v2.myimport.MyImportSelector;
 import com.winson.spring.aop.overviewv2.DefaultEchoService;
 import com.winson.spring.aop.overviewv2.EchoService;
 import org.springframework.context.annotation.*;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 /**
  * @author winson
@@ -18,6 +22,18 @@ import org.springframework.context.annotation.*;
 @EnableAspectJAutoProxy
 public class AspectJAnnotationDemo {
 
+//    @Transactional
+//    public void test(){
+//        TransactionSynchronizationManager.initSynchronization();
+//        System.out.println("demo do test!");
+//        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+//            @Override
+//            public void afterCommit() {
+//                System.out.println("test :: afterCommit");
+//            }
+//        });
+//    }
+
     public static void main(String[] args) {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
@@ -26,8 +42,12 @@ public class AspectJAnnotationDemo {
 //        context.register(AspectConfiguration.class);
         context.refresh();
 
+
         EchoService echoService = context.getBean(EchoService.class);
         echoService.sayHello("winson");
+
+//        AspectJAnnotationDemo demo = context.getBean(AspectJAnnotationDemo.class);
+//        demo.test();
 
     }
 
---
+++ winson-spring-aop-features_v2/src/main/java/com/winson/spring/aop/features/v2/TransactionalDemo.java
@@ -0,0 +1,168 @@
+package com.winson.spring.aop.features.v2;
+
+import org.reactivestreams.Publisher;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ReactiveAdapter;
+import org.springframework.core.ReactiveTypeDescriptor;
+import org.springframework.transaction.*;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
+import org.springframework.transaction.support.AbstractPlatformTransactionManager;
+import org.springframework.transaction.support.DefaultTransactionStatus;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * @author winson
+ * @date 2022/4/19
+ **/
+@EnableTransactionManagement
+public class TransactionalDemo {
+
+    public class Hello {
+
+    }
+
+
+    private static void testException() {
+        try {
+            TransactionInterceptor ti = new TransactionInterceptor();
+
+            Class clazz = Class.forName("org.springframework.transaction.interceptor.TransactionAspectSupport$ReactiveTransactionSupport");
+            System.out.println("success : " + clazz);
+
+            Class rtdc = Class.forName("org.springframework.core.ReactiveTypeDescriptor");
+            Constructor cc = rtdc.getDeclaredConstructors()[0];
+            cc.setAccessible(true);
+            Object rtd = cc.newInstance(TransactionalDemo.class, true, true, new Supplier<Object>() {
+                @Override
+                public Object get() {
+                    return new Object();
+                }
+            });
+//            System.out.println("rtd : " + rtd.getClass());
+            ReactiveTypeDescriptor rr = (ReactiveTypeDescriptor) rtd;
+            ReactiveAdapter adapter = new ReactiveAdapter(rr, new Function<Object, Publisher<?>>() {
+                @Override
+                public Publisher<Object> apply(Object o) {
+                    return null;
+                }
+            }, new Function<Publisher<?>, Object>() {
+                @Override
+                public Object apply(Publisher<?> publisher) {
+                    return new Object();
+                }
+            });
+            Constructor constructor = clazz.getDeclaredConstructors()[0];
+            constructor.setAccessible(true);
+            System.out.println("constructor count : " + constructor.getParameterCount());
+            Object target = constructor.newInstance(ti, adapter);
+            System.out.println("target : " + target);
+            Method method = clazz.getDeclaredMethod("isSuspend", Method.class);
+            System.out.println(method);
+            method.setAccessible(true);
+            method.invoke(target, method);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        KotlinDelegate
+    }
+
+    @Bean
+    public TransactionManager createTransactionManager() {
+//        PlatformTransactionManager transactionManager = new PlatformTransactionManager(){
+//
+//            @Override
+//            public TransactionStatus getTransaction(TransactionDefinition definition) throws TransactionException {
+//                System.out.println("TransactionDefinition --> getTransaction status : " + definition);
+//                TransactionStatus status = new DefaultTransactionStatus(definition,
+//                        true,true, false, true, new Object());
+//                return status;
+//            }
+//
+//            @Override
+//            public void commit(TransactionStatus status) throws TransactionException {
+//                System.out.println("TransactionDefinition --> commit status : " + status);
+//
+//            }
+//
+//            @Override
+//            public void rollback(TransactionStatus status) throws TransactionException {
+//                System.out.println("TransactionDefinition --> rollback status : " + status);
+//
+//            }
+//        };
+//        return transactionManager;
+
+        return new AbstractPlatformTransactionManager() {
+            @Override
+            protected Object doGetTransaction() throws TransactionException {
+                Object obj = new Object();
+                System.out.println(Thread.currentThread() + " , doGetTransaction(): " + obj);
+                return obj;
+            }
+
+            @Override
+            protected void doBegin(Object transaction, TransactionDefinition definition) throws TransactionException {
+                System.out.println(Thread.currentThread() + " , doBegin transaction: " + transaction + " , definition : " + definition);
+            }
+
+            @Override
+            protected void doCommit(DefaultTransactionStatus status) throws TransactionException {
+                System.out.println(Thread.currentThread() + " , doCommit status : " + status);
+                status.setCompleted();
+            }
+
+            @Override
+            protected void doRollback(DefaultTransactionStatus status) throws TransactionException {
+                System.out.println(Thread.currentThread() + " , doRollback status : " + status);
+                status.setRollbackOnly();
+            }
+        };
+    }
+
+    @Transactional
+    public void testTransaction() {
+//        TransactionSynchronizationManager.initSynchronization();
+        System.out.println("demo do test!");
+        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+            @Override
+            public void afterCommit() {
+                System.out.println("test :: afterCommit");
+            }
+        });
+    }
+
+    public void normalAction(){
+        System.out.println("This is normal action ... ");
+    }
+
+    public static void main(String[] args) {
+//        try {
+//            Class.forName("reactor.core.publisher.Flux");
+////            Class.forName("com.winson.spring.aop.features.v2.TransactionalDemo");
+//            System.out.println("success");
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(TransactionalDemo.class);
+        context.refresh();
+
+        TransactionalDemo demo = context.getBean(TransactionalDemo.class);
+        demo.testTransaction();
+        System.out.println("----------");
+        demo.normalAction();
+
+//        testException();
+
+    }
+
+}
