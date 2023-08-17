c5049ca16fb3bdd47aa6db0a82282999e1f416d3
mikeian <zhouwenxiang@ciwei.net>
2023-08-17 12:15:39 +0800

    新增spring测试项目

---
+++ ian-sp-bean-overview/src/main/java/com/ian/sp/bean/overview/OverviewDemo.java
@@ -0,0 +1,18 @@
+package com.ian.sp.bean.overview;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author mike ian
+ * @date 2023/8/17
+ * @desc
+ **/
+public class OverviewDemo {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("ian-sp-bean-overview.xml");
+
+    }
+
+}
