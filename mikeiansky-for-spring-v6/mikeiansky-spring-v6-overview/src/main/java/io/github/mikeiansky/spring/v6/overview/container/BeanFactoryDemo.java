7974d24915b51c330bebb81cef7abdf3fe8f7a83
mikeian <zhouwenxiang@ciwei.net>
2024-12-05 10:45:45 +0800

    add spring v6 overview module

---
+++ mikeiansky-for-spring-v6/mikeiansky-spring-v6-overview/src/main/java/io/github/mikeiansky/spring/v6/overview/container/BeanFactoryDemo.java
@@ -0,0 +1,26 @@
+package io.github.mikeiansky.spring.v6.overview.container;
+
+import io.github.mikeiansky.spring.v6.overview.domain.User;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+/**
+ * @author mike ian
+ * @date 2023/8/17
+ * @desc
+ **/
+public class BeanFactoryDemo {
+
+    public static void main(String[] args) {
+
+        String resourcePath = "classpath:mikeiansky-spring-v6-overview.xml";
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        reader.loadBeanDefinitions(resourcePath);
+
+        User user = factory.getBean(User.class);
+
+        System.out.println(user);
+    }
+
+}
---
+++ mikeiansky-for-spring-v6/mikeiansky-spring-v6-overview/src/main/java/io/github/mikeiansky/spring/v6/overview/domain/User.java
@@ -0,0 +1,17 @@
+package io.github.mikeiansky.spring.v6.overview.domain;
+
+import lombok.Data;
+
+/**
+ * @author mike ian
+ * @date 2023/8/17
+ * @desc
+ **/
+@Data
+public class User {
+
+    private int id;
+    private String name;
+    private int age;
+
+}
