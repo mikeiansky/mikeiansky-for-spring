0bc8edc33fdd237e65afe3a4cb44bf5cbf81bb67
mikeian <zhouwenxiang@ciwei.net>
2024-12-06 18:42:07 +0800

    test spring base api

--- mikeiansky-for-spring-v6/mikeiansky-for-spring-v6-overview/src/main/java/io/github/mikeiansky/spring/v6/overview/container/BaseBeanFactoryDemo.java
+++ mikeiansky-for-spring-v6/mikeiansky-for-spring-v6-overview/src/main/java/io/github/mikeiansky/spring/v6/overview/container/BaseBeanFactoryDemo.java
@@ -2,7 +2,6 @@ package io.github.mikeiansky.spring.v6.overview.container;
 
 import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.factory.annotation.AnnotatedGenericBeanDefinition;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 
 /**
@@ -10,33 +9,48 @@ import org.springframework.beans.factory.support.DefaultListableBeanFactory;
  * @date 2024/12/5
  * @desc
  **/
-public class AnnotationBeanFactoryDemo {
+public class BaseBeanFactoryDemo {
 
     private String tag;
 
+    private Other otherField;
+
     public void setTag(String tag) {
         this.tag = tag;
+    }
+
+    public void setOtherField(Other otherField) {
+        this.otherField = otherField;
     }
 
     public static class Other {
 
     }
 
+    @Override
+    public String toString() {
+        return "AnnotationBeanFactoryDemo{" +
+                "tag='" + tag + '\'' +
+                ", otherField=" + otherField +
+                '}';
+    }
+
     public static void main(String[] args) {
         DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
 
-//        AnnotatedGenericBeanDefinition otherBeanDefinition = new AnnotatedGenericBeanDefinition(Other.class);
+        AnnotatedGenericBeanDefinition otherBeanDefinition = new AnnotatedGenericBeanDefinition(Other.class);
 //        factory.registerBeanDefinition(Other.class.getName(), otherBeanDefinition);
 
-        AnnotatedGenericBeanDefinition fatherBeanDefinition = new AnnotatedGenericBeanDefinition(AnnotationBeanFactoryDemo.class);
+        AnnotatedGenericBeanDefinition fatherBeanDefinition = new AnnotatedGenericBeanDefinition(BaseBeanFactoryDemo.class);
 //        fatherBeanDefinition.setDependsOn(Other.class.getName());
         MutablePropertyValues mutablePropertyValues = new MutablePropertyValues();
         mutablePropertyValues.addPropertyValue("tag", "demo");
+        mutablePropertyValues.addPropertyValue("otherField", otherBeanDefinition);
         fatherBeanDefinition.setPropertyValues(mutablePropertyValues);
 //        fatherBeanDefinition.setDependsOn(Other.class.getName());
-        factory.registerBeanDefinition(AnnotationBeanFactoryDemo.class.getName(), fatherBeanDefinition);
+        factory.registerBeanDefinition(BaseBeanFactoryDemo.class.getName(), fatherBeanDefinition);
 
-        AnnotationBeanFactoryDemo father = factory.getBean(AnnotationBeanFactoryDemo.class);
+        BaseBeanFactoryDemo father = factory.getBean(BaseBeanFactoryDemo.class);
         System.out.println(father);
         System.out.println(father.tag);
 
