20450f85790011bb9f1bed31e69bb3e5ff8f6c00
mikeian <zhouwenxiang@ciwei.net>
2024-12-13 14:37:59 +0800

    add spring annotation utils test

---
+++ mikeiansky-for-spring-v6/mikeiansky-for-spring-v6-overview/src/main/java/io/github/mikeiansky/spring/v6/overview/annotation/AliasForAnnotatedElementUtilsDemo.java
@@ -0,0 +1,50 @@
+package io.github.mikeiansky.spring.v6.overview.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author mike ian
+ * @date 2024/12/13
+ * @desc
+ **/
+public class AliasForAnnotatedElementUtilsDemo {
+
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface FatherAnnotation {
+
+        String helloFather() default "hello-father";
+
+    }
+
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    @FatherAnnotation   // 这里必须要做
+    public @interface SonAnnotation {
+
+        @AliasFor(annotation = FatherAnnotation.class, value = "helloFather")
+        String helloSon() default "hello-son";
+
+    }
+
+    @SonAnnotation(helloSon = "hello-son-one")
+    public static class One {
+
+
+
+    }
+
+    public static void main(String[] args) {
+        FatherAnnotation fatherAnnotation = AnnotatedElementUtils.findMergedAnnotation(One.class, FatherAnnotation.class);
+        System.out.println("fatherAnnotation :: " + fatherAnnotation);
+        SonAnnotation sonAnnotation = AnnotatedElementUtils.findMergedAnnotation(One.class, SonAnnotation.class);
+        System.out.println("sonAnnotation :: " + sonAnnotation);
+    }
+
+}
