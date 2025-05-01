263a8c958755f5c7711e06293247fd0f8c40cc14
ian <929566854@qq.com>
2025-05-01 13:22:19 +0800

    Test circle reference for bean factory;

--- mikeiansky-for-spring-v6/mikeiansky-for-spring-v6-overview/src/main/java/io/github/mikeiansky/spring/v6/overview/factory/dependency/injection/MoreThanTwoCircleReferenceDemo.java
+++ mikeiansky-for-spring-v6/mikeiansky-for-spring-v6-overview/src/main/java/io/github/mikeiansky/spring/v6/overview/factory/dependency/injection/MoreThanTwoCircleReferenceDemo.java
@@ -5,12 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.annotation.ContextAnnotationAutowireCandidateResolver;
 import org.springframework.context.annotation.Lazy;
 
 /**
  * @author mike ian
  * @date 2025/4/30
- * @desc
+ * @desc 也就是在一个bean是通过构造函数进行参数注入的时候， 这个时候这个bean还没有创建
+ * ，但是它用于依赖注入的bean又依赖了这个为创建的bean，则会出现循环引用问题，这个时候需要
+ * 通过给BeanFactory配置能够解析Lazy bean的resolver来进行懒加载
  **/
 public class MoreThanTwoCircleReferenceDemo {
 
@@ -26,7 +29,7 @@ public class MoreThanTwoCircleReferenceDemo {
 
     public static class Two {
 
-        @Lazy
+//        @Lazy
         @Autowired
         private Three three;
 
@@ -46,6 +49,8 @@ public class MoreThanTwoCircleReferenceDemo {
         AutowiredAnnotationBeanPostProcessor beanPostProcessor = new AutowiredAnnotationBeanPostProcessor();
         beanPostProcessor.setBeanFactory(beanFactory);
         beanFactory.addBeanPostProcessor(beanPostProcessor);
+        // 解决循环引用的关键节点
+        beanFactory.setAutowireCandidateResolver(new ContextAnnotationAutowireCandidateResolver());
 
         RootBeanDefinition oneBean = new RootBeanDefinition(One.class);
         beanFactory.registerBeanDefinition("one", oneBean);
@@ -64,7 +69,8 @@ public class MoreThanTwoCircleReferenceDemo {
         System.out.println("two : " + two);
         System.out.println("two.three : " + two.three);
         System.out.println("three : " + three);
-        System.out.println("three.one : " + three);
+        System.out.println("three.one : " + three.one);
+        System.out.println("three.one : " + three.one);
 
     }
 
