1902ecbf307f613630ac5b8da725b2c619f98705
mikeian <zhouwenxiang@ciwei.net>
2025-05-26 12:12:25 +0800

    Test enhance for spring aop;

---
+++ mikeiansky-for-spring-v6/mikeiansky-for-spring-v6-aop-overview/src/main/java/io/github/mikeiansky/spring/v6/aop/base/CglibProxyDemo.java
@@ -0,0 +1,75 @@
+package io.github.mikeiansky.spring.v6.aop.base;
+
+import org.springframework.cglib.proxy.*;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author mike ian
+ * @date 2025/5/26
+ * @desc
+ **/
+public class CglibProxyDemo {
+
+    public static class One {
+        public void hello(){
+            System.out.println("one say hello");
+        }
+        public void good(){
+            System.out.println("one say good");
+        }
+    }
+
+    public static void main(String[] args) {
+        AtomicInteger count = new AtomicInteger(0);
+        Enhancer enhancer = new Enhancer();
+        enhancer.setClassLoader(One.class.getClassLoader());
+        enhancer.setSuperclass(One.class);
+        enhancer.setCallbackFilter(new CallbackFilter() {
+
+            @Override
+            public int accept(Method method) {
+
+                int index = count.getAndIncrement();
+                System.out.println("accept index : " + index + ", method : " + method.getName());
+                return index % 3; // 返回0,1,2
+            }
+        });
+        enhancer.setCallbacks(new Callback[] {
+//                new NoOp(){},
+                new MethodInterceptor() {
+
+                    @Override
+                    public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                        System.out.println("method interceptor 01 invoke method : " + method.getName());
+                        return proxy.invokeSuper(obj, args);
+                    }
+                },
+                new MethodInterceptor() {
+
+                    @Override
+                    public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                        System.out.println("method interceptor 02 invoke method : " + method.getName());
+                        return proxy.invokeSuper(obj, args);
+                    }
+                },
+                new MethodInterceptor() {
+
+                    @Override
+                    public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                        System.out.println("method interceptor 03 invoke method : " + method.getName());
+                        return proxy.invokeSuper(obj, args);
+                    }
+                }
+        });
+        One one = (One) enhancer.create();
+//        System.out.println("one : " + one);
+        System.out.println(one.getClass());
+        System.out.println(one.getClass().getClassLoader());
+        one.hello();
+        one.good();
+
+    }
+
+}
