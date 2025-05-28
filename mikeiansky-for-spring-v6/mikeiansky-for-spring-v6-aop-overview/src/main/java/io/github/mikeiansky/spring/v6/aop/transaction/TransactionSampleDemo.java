d0b4c45a07914764e8cc172c1930970e4485485c
mikeian <zhouwenxiang@ciwei.net>
2025-05-28 15:15:42 +0800

    Test spring transactional;

--- mikeiansky-for-spring-v6/mikeiansky-for-spring-v6-aop-overview/src/main/java/io/github/mikeiansky/spring/v6/aop/transaction/TransactionSampleDemo.java
+++ mikeiansky-for-spring-v6/mikeiansky-for-spring-v6-aop-overview/src/main/java/io/github/mikeiansky/spring/v6/aop/transaction/TransactionSampleDemo.java
@@ -1,9 +1,18 @@
 package io.github.mikeiansky.spring.v6.aop.transaction;
 
+import org.aopalliance.intercept.MethodInvocation;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.aop.interceptor.ExposeInvocationInterceptor;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.*;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.SimpleTransactionStatus;
+
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * @author mike ian
@@ -11,6 +20,30 @@ import org.springframework.transaction.annotation.Transactional;
  * @desc
  **/
 public class TransactionSampleDemo {
+
+//    @Aspect
+//    public static class MyAspect {
+//
+//        public MyAspect() {
+////            System.out.println("create MyAspect");
+//        }
+//
+//        @Pointcut("execution(public * *(..))")
+//        public void anyPublic() {
+//            System.out.println("use any public");
+//        }
+//
+//        @Before("anyPublic()")
+//        public void beforeAnyPublic() throws InvocationTargetException, IllegalAccessException {
+//            System.out.println("============> start beforeAnyPublic this : " + this);
+//            MethodInvocation currentInvocation = ExposeInvocationInterceptor.currentInvocation();
+////            System.out.println("currentInvocation : " + currentInvocation);
+////            System.out.println("currentInvocation.getThis() : " + currentInvocation.getThis());
+//            System.out.println("currentInvocation.getMethod() : " + currentInvocation.getMethod());
+////            currentInvocation.getMethod().invoke(currentInvocation.getThis(), currentInvocation.getArguments());
+////            System.out.println("complete beforeAnyPublic this : " + this);
+//        }
+//    }
 
     @EnableTransactionManagement
     public static class One {
@@ -22,12 +55,18 @@ public class TransactionSampleDemo {
 
     }
 
+//    @EnableAspectJAutoProxy
+    public static class MyConfig{
+
+    }
+
 
     public static class MyTransactionManager implements PlatformTransactionManager {
 
         @Override
         public TransactionStatus getTransaction(TransactionDefinition definition) throws TransactionException {
             System.out.println("getTransaction : " + definition);
+//            return new SimpleTransactionStatus();
             return null;
         }
 
@@ -44,6 +83,8 @@ public class TransactionSampleDemo {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+//        context.register(MyConfig.class);
+//        context.register(MyAspect.class);
         context.register(One.class);
         context.register(MyTransactionManager.class);
         context.refresh();
